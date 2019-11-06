43151537e966f7e009dd14a956de1f0b62774e00
twoody <tanner.l.woody@gmail.com>
2019-11-05 21:20:58 -0800

    starting point for broken app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-cal
+      :time-cell-height="18"
+      style="height: 350px"
+      default-view="day"
+      :disable-views="['years', 'year', 'month']"
+      :events="events"
+      :time-from="8 * 60"
+      :time-to="20 * 60"
+      :time-step="1 * 60"
+      :selected-date="'2018-11-21'"
+      hide-weekends
+     >
+      <template v-slot:time-cell="{ hours }">
+        <div :class="{ line: true }">
+          {{ hours }}
+        </div>
+      </template>
+    </vue-cal>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components:
+  {
+    VueCal
+  },
+  data: function () {
+    return {
+    events: [
+      {
+        start: '2018-11-21',
+        end: '2018-11-21',
+        title: 'Need to go shopping',
+        content: '<i class="v-icon material-icons">shopping_cart</i>',
+        class: 'leisure'
+      },
+      {
+        start: '2018-11-21',
+        end: '2018-11-21',
+        title: 'Golf with John',
+        content: '<i class="v-icon material-icons">golf_course</i>',
+        class: 'sport'
+      },
+      {
+        start: '2018-11-22',
+        end: '2018-11-22',
+        title: 'Dad\'s birthday!',
+        content: '<i class="v-icon material-icons">cake</i>',
+        class: 'sport'
+      }
+    ]
+  }},
+  created: function (){
+  },//end created
+  mounted () {
+  },
+
+  methods:
+  {
+  }//end methods
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  .vuecal {
+    box-shadow: none;
+  }
+  .vuecal__time-cell {
+    .line {
+    /* margin-left: 15px; */
+    }
+    .line:before{
+      margin-left:55px;
+    /* border-top: none !important; */
+    }
+  }
 </style>
